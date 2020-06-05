8e573c8e1cd9240a0c7fc4caf4d95c1376a14c40
chooob <61262919+chooob@users.noreply.github.com>
2020-06-04 18:06:59 -0700

    Updates

---
+++ precache-manifest.5e626e1d6c7e6caa77cef33fb1d63a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92ea6beb3eb87bd1c93f7e270e9318c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7235b8484b12c3e702e9",
+    "url": "/static/css/main.238cb3b1.chunk.css"
+  },
+  {
+    "revision": "576239bdd47165c3f765",
+    "url": "/static/js/2.f88abdb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f88abdb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7235b8484b12c3e702e9",
+    "url": "/static/js/main.733e208a.chunk.js"
+  },
+  {
+    "revision": "c94d3d9487f5bf5910f3",
+    "url": "/static/js/runtime-main.2d53f70b.js"
+  }
+]);
